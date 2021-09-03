8962cd081cb871a1a6de17f9df536c97e006e484
ZulZayn <zulwaqarzain96@gmail.com>
2021-09-03 12:38:44 +0800

    service worker update

--- core/js/serviceworker_loader.js
+++ core/js/serviceworker_loader.js
@@ -1,5 +1,18 @@
+// if ('serviceWorker' in navigator) {
+//     window.addEventListener('load', function() {
+//         navigator.serviceWorker.register(OC.getRootPath() + '/core/js/serviceworker.js', {scope: OC.getRootPath() + '/'});
+//     });
+// }
+
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register(OC.getRootPath() + '/core/js/serviceworker.js', {scope: OC.getRootPath() + '/'});
+        navigator.serviceWorker
+        .register('/serviceworker.js')
+        .then(function () {
+        console.log('Service worker registered!');
+        })
+        .catch(function(err) {
+        console.log(err);
+        });
     });
-}
+    }
