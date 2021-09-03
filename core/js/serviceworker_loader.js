e7982bcc59900aca67267caa516cf258cbb84a9d
ZulZayn <zulwaqarzain96@gmail.com>
2021-09-03 16:02:42 +0800

    service worker update

--- core/js/serviceworker_loader.js
+++ core/js/serviceworker_loader.js
@@ -7,7 +7,7 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
         navigator.serviceWorker
-        .register('/serviceworker.js')
+        .register('../../serviceworker.js')
         .then(function () {
         console.log('Service worker registered!');
         })
