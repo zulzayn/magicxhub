b22da5ff9b551aa0f9be56c5c1c050d94a656488
ZulZayn <zulwaqarzain96@gmail.com>
2021-09-04 11:55:57 +0800

    firstrunwizard pagevalues

--- apps/firstrunwizard/templates/page.values.php
+++ apps/firstrunwizard/templates/page.values.php
@@ -31,27 +31,44 @@
 <div class="page" data-title="<?php p($l->t('A safe home for all your data')); ?>" data-subtitle="">
 	<div class="content content-values">
 		<p>
-			<?php p($l->t('Nextcloud puts your data at your fingertips, under your control. Store your documents, calendar, contacts and photos on a server in your company, at home, at one of our providers or in a data center you know.')); ?>
+			<?php p($l->t('Introducing MaGICXHub, the central hub of your digital life. 
+			MaGICXHub puts your data at your fingertips, under your control. 
+			Share and collaborate on documents, 
+			send and receive email, manage your contacts and calendar, 
+			have chats and video chats without data leaks, 
+			make an online conference with your worker,
+			make a virtual classroom for lecturing. 
+			All built from the ground up.
+			Everything you need to get anything done, now in one place.')); ?>
 		</p>
 
 		<ul id="wizard-values">
 			<li>
-				<span class="icon-link"></span>
-				<h3><?php p($l->t('Host your data and files where you decide')); ?></h3>
+				<span class="icon-files"></span>
+				<p><?php p($l->t('Files offers an on-premise Universal File Access and sync platform with powerful collaboration capabilities within devices.')); ?></p>
 			</li>
 			<li>
-				<span class="icon-shared"></span>
-				<h3><?php p($l->t('Open Standards and Interoperability')); ?></h3>
+				<span class="icon-category-dashboard"></span>
+				<p><?php p($l->t('Talk delivers on-premises, private audio/video conferencing and text chat through browser and mobile interfaces with integrated screen sharing and SIP integration.')); ?></p>
 			</li>
 			<li>
-				<span class="icon-user"></span>
-				<h3><?php p($l->t('100%% Open Source & community-focused')); ?></h3>
+				<span class="icon-category-social"></span>
+				<p><?php p($l->t('Groupware integrates Calendar, Contacts, Mail and other productivity features to help teams get their work done faster, easier and on your terms.')); ?></p>
+			</li>
+			<li>
+				<span class="icon-stackexchange"></span>
+				<p><?php p($l->t('Collaborative platform in virtual workspace to facilitating communication and personal interaction in any project work.')); ?></p>
+			</li>
+			<li>
+				<span class="icon-desktop"></span>
+				<p><?php p($l->t('Combine business and education virtual conference into a single platform. Experience secure, productive conference from wherever you are. Lecture from any part of the world using classroom features.')); ?></p>
 			</li>
 		</ul>
 
 		<p class="details-link"><a href="<?php p($theme->getBaseUrl()); ?>" target="_blank" rel="noreferrer noopener"><?php p($l->t('Learn more about %s', $theme->getName())); ?></a></p>
 		<div style="text-align: center; color: var(--text-maxcontrast);">
-			<p><?php p($l->t('This Nextcloud is on version %s', [ \OC_Util::getVersionString() ])); ?></p>
+			<p><?php p($l->t('This MaGICXHub is on version %s', [ \OC_Util::getVersionString() ])); ?></p>
 		</div>
 	</div>
 </div>
+
